3baa87f28b5b5f60a2ffbe3b2cb20d9b51e3a11e
Manu <mcabral@frba.utn.edu.ar>
2021-12-12 19:01:29 -0300

    fixed lang query bug

--- src/TwitchRPC.py
+++ src/TwitchRPC.py
@@ -8,26 +8,27 @@ from os import system
 from sys import exit
 from json import loads
 from constants import *
-
 class Browser:
-    def __init__(self, args):
+    def __init__(self, args, lang):
         self.name = args[0]
         self.full_name = args[1]
         self.process_name = args[2]
+        self.lang = lang
 
     def running(self):
         return True if self.name.lower() in check_output(f'wmic process where "name=\'{self.process_name}\'" get ExecutablePath', universal_newlines=True, stderr=PIPE).lower() else False
 
     def current_website(self):
         token = "- Twitch"
-        title = check_output(f'tasklist.exe /fi "imagename eq {self.process_name}" /fo list /v | find "Window Title:"', shell=True, universal_newlines=True, stderr=PIPE).split('\n')[0]
-        return title[14:] if token in check_output(f'tasklist.exe /fi "imagename eq {self.process_name}" /fo list /v | find "Window Title:"', shell=True, universal_newlines=True, stderr=PIPE).split('\n')[0] else False
-
+        field = "Window Title:" if "en" in self.lang else "Título de ventana:"
+        title = check_output(f'tasklist.exe /fi "imagename eq {self.process_name}" /fo list /v | find "{field}"', shell=True, universal_newlines=True, stderr=PIPE).split('\n')[0]
+        return title[14:] if token in check_output(f'tasklist.exe /fi "imagename eq {self.process_name}" /fo list /v | find "{field}"', shell=True, universal_newlines=True, stderr=PIPE).split('\n')[0] else False
 class TwitchRPC:
     def __init__(self, client_id):
         self.client_id = client_id
         self.toast = ToastNotifier()
         self.browser = self.rpc = self.running = self.prev_streamer = self.start_time = None
+        self.lang = self.full_lang = None
 
     def display_logo(self):
         print(BANNER)
@@ -35,7 +36,7 @@ class TwitchRPC:
         self.message(Fore.WHITE, f'Author: {Fore.YELLOW}{AUTHOR}')
         print("")
 
-    def init_settngs(self):
+    def init_settings(self):
         init()
         system('title ' + NAME)
         system(F'mode con:cols={COLUMNS} lines={LINES}')
@@ -60,7 +61,6 @@ class TwitchRPC:
             self.message(Fore.LIGHTYELLOW_EX, f'New update is available > {Fore.CYAN}{versions[0]}{Fore.LIGHTYELLOW_EX}, please update.')
         else:
             self.message(Fore.GREEN, "Up to date")
-
 
     def get_url(self, title):
         return TWITCH_URL + title.split(' ')[0]
@@ -90,6 +90,8 @@ class TwitchRPC:
                     return option
             except (ValueError, TypeError):
                 self.message(Fore.RED, 'Please pick a number')
+            except KeyboardInterrupt:
+                self.stop()
 
     def browser_handler(self, browsers):
         if len(browsers) == 0:
@@ -99,7 +101,7 @@ class TwitchRPC:
         if len(browsers) == 1:
             browser = browsers[0]
             self.message(Fore.GREEN, f'{browser[1]} located!')
-            self.browser = Browser(browser)
+            self.browser = Browser(browser, self.lang)
 
         if len(browsers) > 1:
             self.message(Fore.LIGHTCYAN_EX, f'Currently {len(browsers)} browsers running..')
@@ -107,7 +109,7 @@ class TwitchRPC:
                 self.message( Fore.WHITE, f'{Fore.CYAN}[{i}]{Fore.RESET} {browser[1]}')
             option = self.select_browser(len(browsers))
             self.message(Fore.CYAN, f'You selected: {browsers[option][1]}')
-            self.browser = Browser(browsers[option])
+            self.browser = Browser(browsers[option], self.lang)
 
     def connect_rpc(self):
         try:
@@ -119,7 +121,6 @@ class TwitchRPC:
         except Exception as e:
             self.message(Fore.RED, e)
         self.message(Fore.LIGHTGREEN_EX, 'Successfully connected', toast=True)
-
 
     def get_token(self, title):
         for token in TOKENS[0]:
@@ -172,10 +173,20 @@ class TwitchRPC:
         system('pause > nul')
         exit()
 
+    def get_system_lang(self):
+        lang = check_output('powershell get-uiculture', universal_newlines=True, stderr=PIPE).split('\n')[3][17:].split('           ')
+        self.lang = lang[0]
+        self.full_lang = lang[1][1:]
+        if not self.lang:
+            self.message(Fore.RED, "Error on get your system language")
+        self.message(Fore.GREEN, f"Language detected {self.full_lang}")
+    
     def run(self):
         system('cls')
         if not self.running:
-            self.init_settngs()
+            self.init_settings()
+            self.message(Fore.YELLOW, 'Checking your system language..')
+            self.get_system_lang()
             self.message(Fore.YELLOW, 'Checking updates..')
             self.check_update()
             self.message(Fore.YELLOW, 'Scanning browsers on your system..')
